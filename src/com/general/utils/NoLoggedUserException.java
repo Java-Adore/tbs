7e714fcaf50de6f5776cf56411eaf7e7c921ddf5
Mahmoud Eltaieb <mahmoud.eltaieb@gmail.com>
2015-01-09 23:26:07 +0200

    login

--- src/com/general/utils/NoLoggedUserException.java
+++ src/com/general/utils/NoLoggedUserException.java
@@ -3,7 +3,6 @@ package com.general.utils;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-
 public class NoLoggedUserException extends RuntimeException  implements Serializable{
 
 	/**
